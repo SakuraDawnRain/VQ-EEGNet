3bf5d7f29a3c3380ac5c858034927c4f3ccab33b
SakuraDawnRain <SakuraDawnRain@outlook.com>
2025-02-18 14:20:59 +0800

    update preprocess

--- preprocess.py
+++ preprocess.py
@@ -2,6 +2,8 @@ import os
 import mne
 import numpy as np
 import scipy
+import torch
+from sklearn.model_selection import train_test_split
 
 
 raw_data_folder = 'raw_data'
@@ -101,3 +103,44 @@ final_raw = mne.concatenate_raws(raw_list)
 new_file_path = os.path.join(cleaned_data_folder, 'Second_Session_Subjects.fif')
 final_raw.save(new_file_path, overwrite=True)
 np.save(os.path.join(cleaned_data_folder, 'Second_Session_Labels.npy'), second_session_labels)
+
+first_raw = mne.io.read_raw("cleaned_data/first_session/First_Session_Subjects.fif")
+first_events = mne.events_from_annotations(first_raw)
+epochs = mne.Epochs(first_raw, first_events[0], event_id=[7, 8, 9, 10], tmin=0, tmax=4, reject=None, baseline=None, preload=True)
+first_session_data = epochs.get_data(copy=True)
+first_session_labels = epochs.events[:,-1]
+
+second_raw = mne.io.read_raw("cleaned_data/second_session/Second_Session_Subjects.fif")
+second_events = mne.events_from_annotations(second_raw)
+epochs = mne.Epochs(second_raw, second_events[0], event_id=7, tmin=0, tmax=4, reject=None, baseline=None, preload=None)
+second_session_data = epochs.get_data(copy=True)
+second_session_labels = np.load("cleaned_data/second_session/Second_Session_Labels.npy")
+
+# Normalizing Labels to [0, 1, 2, 3]
+y_train = first_session_labels - np.min(first_session_labels)
+y_test = second_session_labels - np.min(second_session_labels)
+
+# Normalizing Input features: z-score(mean=0, std=1)
+X_first_session = (first_session_data - np.mean(first_session_data)) / np.std(first_session_data)
+X_second_session = (second_session_data - np.mean(second_session_data)) / np.std(second_session_data)
+
+final_raw = mne.concatenate_raws(raw_list)
+new_file_path = os.path.join(cleaned_data_folder, 'Second_Session_Subjects.fif')
+final_raw.save(new_file_path, overwrite=True)
+np.save(os.path.join(cleaned_data_folder, 'Second_Session_Labels.npy'), second_session_labels)
+
+X = np.concatenate((X_first_session, X_second_session))
+y = np.concatenate((y_train, y_test))
+
+# Spliting  Data: 90% for Train and 10% for Test
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1, random_state=42, stratify=y)
+
+# Converting to Tensor
+X_train = torch.Tensor(X_train).unsqueeze(1)
+torch.save(X_train, "X_train.pt")
+X_test = torch.Tensor(X_test).unsqueeze(1)
+torch.save(X_test, "X_test.pt")
+y_train = torch.LongTensor(y_train)
+torch.save(y_train, "y_train.pt")
+y_test = torch.LongTensor(y_test)
+torch.save(y_test, "y_test.pt")
